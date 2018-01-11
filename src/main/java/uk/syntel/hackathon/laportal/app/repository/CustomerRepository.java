93d5311c7b23db1188098505bd31a9bbaffa49b8
amit2067 <amit2067@gmail.com>
2018-01-11 09:35:20 +0000

    Update CustomerRepository.java

--- src/main/java/uk/syntel/hackathon/laportal/app/repository/CustomerRepository.java
+++ src/main/java/uk/syntel/hackathon/laportal/app/repository/CustomerRepository.java
@@ -1,5 +1,7 @@
 package uk.syntel.hackathon.laportal.app.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import uk.syntel.hackathon.laportal.app.beans.Customer;
 
 @Repository
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
+	
+	List<Customer> findAll();
 
 }
