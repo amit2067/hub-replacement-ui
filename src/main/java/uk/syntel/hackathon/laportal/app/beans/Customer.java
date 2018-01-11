3ca1c6ba8596d161111af79fb4c7ab637a0042e6
amit2067 <amit2067@gmail.com>
2018-01-11 09:33:07 +0000

    Update Customer.java

--- src/main/java/uk/syntel/hackathon/laportal/app/beans/Customer.java
+++ src/main/java/uk/syntel/hackathon/laportal/app/beans/Customer.java
@@ -2,31 +2,53 @@ package uk.syntel.hackathon.laportal.app.beans;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import io.swagger.annotations.ApiModelProperty;
 
 @Entity
 public class Customer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CUST_SEQ")
-    @SequenceGenerator(sequenceName = "customer_seq", allocationSize = 1, name = "CUST_SEQ")
+    @GenericGenerator(name = "incrementGenerator", strategy = "org.hibernate.id.IncrementGenerator")
+    @GeneratedValue(generator="incrementGenerator")
+    @ApiModelProperty(notes="Customer Id")
 	private Long id;
+    
+    @ApiModelProperty(notes="First name")
 	private String firstName;
+    
+    @ApiModelProperty(notes="Last name")
 	private String lastName;
+    
+    @ApiModelProperty(notes="Email Address")
 	private String emailAddress;
+    
+    @ApiModelProperty(notes="Address Line 1")
 	private String addressLine1;
+    
+    @ApiModelProperty(notes="Address Line 2")
 	private String addressLine2;
+    
+    @ApiModelProperty(notes="City")
 	private String city;
+    
+    @ApiModelProperty(notes="Postal Code")
 	private String postalCode;
+    
+    @ApiModelProperty(notes="Phone Number")
 	private String phoneNumber;
+    
+    @ApiModelProperty(notes="Created By")
+	private String createdBy;
 
 	public Customer() {
 	}
 
 	public Customer(Long id, String firstName, String lastName, String emailAddress, String addressLine1,
-			String addressLine2, String city, String postalCode, String phoneNumber) {
+			String addressLine2, String city, String postalCode, String phoneNumber, String createdBy) {
 		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -36,6 +58,7 @@ public class Customer {
 		this.city = city;
 		this.postalCode = postalCode;
 		this.phoneNumber = phoneNumber;
+		this.createdBy = createdBy;
 	}
 
 	public Long getId() {
@@ -110,13 +133,19 @@ public class Customer {
 		this.phoneNumber = phoneNumber;
 	}
 
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
 	@Override
 	public String toString() {
 		return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", emailAddress="
 				+ emailAddress + ", addressLine1=" + addressLine1 + ", addressLine2=" + addressLine2 + ", city=" + city
-				+ ", postalCode=" + postalCode + ", phoneNumber=" + phoneNumber + "]";
+				+ ", postalCode=" + postalCode + ", phoneNumber=" + phoneNumber + ", createdBy=" + createdBy + "]";
 	}
-	
-	
 
 }
