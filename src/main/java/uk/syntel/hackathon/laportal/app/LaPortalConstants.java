2ea3d0007890236cd7e9d031af12c872c57a712c
amit2067 <amit2067@gmail.com>
2018-01-11 09:54:59 +0000

    Create LaPortalConstants.java

---
+++ src/main/java/uk/syntel/hackathon/laportal/app/LaPortalConstants.java
@@ -0,0 +1,8 @@
+package uk.syntel.hackathon.laportal.app.util;
+
+public interface LaPortalConstants {
+	String CUSTOMER_PUBLISH_URL = "http://CZC3392FZ7.SLC.CO.UK:9205/kpublisher/publishCust";
+	String APPLICATION_PUBLISH_URL = "http://CZC3392FZ7.SLC.CO.UK:9205/kpublisher/publishApp";
+	String LA_PORTAL = "LaPortal";
+	String ELASTIC_PUBLISH_URL = "http://czc4464xb3.slc.co.uk:9183/createLog";
+}
