fae171f4fec6a964033c8c094888e3a6309bf064
amit2067 <amit2067@gmail.com>
2018-01-11 09:33:28 +0000

    Update Application.java

--- src/main/java/uk/syntel/hackathon/laportal/app/beans/Application.java
+++ src/main/java/uk/syntel/hackathon/laportal/app/beans/Application.java
@@ -2,29 +2,50 @@ package uk.syntel.hackathon.laportal.app.beans;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import io.swagger.annotations.ApiModelProperty;
 
 @Entity
 public class Application {
 
 	@Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "APP_SEQ")
-    @SequenceGenerator(sequenceName = "application_seq", allocationSize = 1, name = "APP_SEQ")
+	@GenericGenerator(name = "incrementGenerator", strategy = "org.hibernate.id.IncrementGenerator")
+    @GeneratedValue(generator="incrementGenerator")
+	@ApiModelProperty(notes="Application Id")
 	private Long id;
+	
+	@ApiModelProperty(notes="Customer Id")
 	private Long customerId;
+	
+	@ApiModelProperty(notes="HEI Code")
 	private String heiCode;
+	
+	@ApiModelProperty(notes="Course Code")
 	private String courseCode;
+	
+	@ApiModelProperty(notes="Course Year")
 	private String courseYear;
+	
+	@ApiModelProperty(notes="Tuition fee loan amount")
 	private Integer tflAmount;
+	
+	@ApiModelProperty(notes="Maintenance Loan Amount")
 	private Integer mlAmount;
+	
+	@ApiModelProperty(notes="Created By")
+	private String createdBy;
+	
+	@ApiModelProperty(notes="Attendance Confirmed Indicator")
+	private String confirmedAtnInd;
 
 	public Application() {
 	}
 
 	public Application(Long id, Long customerId, String heiCode, String courseCode, String courseYear,
-			Integer tflAmount, Integer mlAmount) {
+			Integer tflAmount, Integer mlAmount, String createdBy, String confirmedAtnInd) {
 		this.id = id;
 		this.customerId = customerId;
 		this.heiCode = heiCode;
@@ -32,6 +53,8 @@ public class Application {
 		this.courseYear = courseYear;
 		this.tflAmount = tflAmount;
 		this.mlAmount = mlAmount;
+		this.createdBy = createdBy;
+		this.confirmedAtnInd = confirmedAtnInd;
 	}
 
 	public Long getId() {
@@ -90,11 +113,27 @@ public class Application {
 		this.mlAmount = mlAmount;
 	}
 
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public String getConfirmedAtnInd() {
+		return confirmedAtnInd;
+	}
+
+	public void setConfirmedAtnInd(String confirmedAtnInd) {
+		this.confirmedAtnInd = confirmedAtnInd;
+	}
+
 	@Override
 	public String toString() {
 		return "Application [id=" + id + ", customerId=" + customerId + ", heiCode=" + heiCode + ", courseCode="
 				+ courseCode + ", courseYear=" + courseYear + ", tflAmount=" + tflAmount + ", mlAmount=" + mlAmount
-				+ "]";
+				+ ", createdBy=" + createdBy + ", confirmedAtnInd=" + confirmedAtnInd + "]";
 	}
 
 }
